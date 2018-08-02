c6a95bc30177ebb46895bb28d9de07e9ecb01fdf
Roel <roeldeking@gmail.com>
2018-08-02 17:03:52 +0200

    cfg file for toggeling the logger

--- parser.sh
+++ parser.sh
@@ -1,16 +1,24 @@
 preexec() {
-    #vragen of de command gelogd moet worden of niet
-    vared -p 'Do you want to log the next command? [y/N]: ' -c tmp
+    file="/home/$USER/.logger.cfg"
+    while IFS= read -r line; do
+    #Check of logger.cfg een 1 bevat
+        if [ $line -eq 1 ] ; then
+            stop=1
+        else
+            stop=0
+        fi
+    done < "$file"
+    #werkt niet
+    if ! [ $stop -eq 1 ]; then
+        #vragen of de command gelogd moet worden of niet
+        vared -p 'Do you want to log this command? [y/N]: ' -c tmp
 
-    case "$tmp" in
-    "y") print "logged";;
-    "n") print "";;
-    *) print "";
-    esac
-
-    #print $1
-
-    
-
+        #cases
+        case "$tmp" in
+        "y") print "logged"; echo $1 >> /home/$USER/Documents/log.txt;;
+        "n") print "";;
+        *) print "";
+        esac
+    fi
 
 }
