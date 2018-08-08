1303aa9a75b26c3ed13b7cc139115d8f188e88e1
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-08-08 23:40:07 +0200

    Minor tweaks to installer
    - Changed print to echo as it tried to print a file to the terminal
    - Changed mv to cp so the original logger.cfg remains for reference

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-print "ZSH MUST BE INSTALLED FOR THIS INSTALLER TO COMPLETE SUCCESSFULLY!!!"
-print "The Forensic Logger is turned off by default. To enable it set logStat=1 in /home/$USER/.logger.cfg"
+#/bin/bash
+echo "ZSH MUST BE INSTALLED FOR THIS INSTALLER TO COMPLETE SUCCESSFULLY!!!"
+echo "The Forensic Logger is turned off by default. To enable it set logStat=1 in /home/$USER/.logger.cfg"
+
 mkdir /home/$USER/Documents/ForensicLogger
-mv .logger.cfg /home/$USER/.logger.cfg
+cp .logger.cfg /home/$USER/.logger.cfg
+
 path=$(pwd)/parser.sh
 echo "source" $path >> /home/$USER/.zshrc
-
