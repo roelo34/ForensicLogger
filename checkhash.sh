4c4f3aa893133ba4de985138d51bcc5af6cb02c7
Roel <roeldeking@gmail.com>
2018-08-14 01:55:38 +0200

    Hash checking script added

---
+++ checkhash.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+fileHashType=$(echo $2 | cut -d. -f2)
+hashingFile=$("$fileHashType"sum "$1" | cut -d ' ' -f 1)
+
+if $hashingFile; then
+    echo 'An error occured, the file containing the hash is probably the wrong format...'
+    echo 'Make sure the extention of the file containing the hash IS the hashing algorithm! i.e. ".sha1"'
+else
+    givenHash=$(cat "$2" | cut -d ' ' -f 1)
+
+    echo 'Please ignore the error above.'
+    echo $hashingFile
+    echo $givenHash
+
+    if [ $hashingFile = $givenHash ]; then
+        echo "Hashes match!"
+    else
+        echo "Hashes DO NOT match!"
+    fi
+fi
